53ec204cb1d671017e36c9a9ed77d7f0eed9767e
Jonathan Phair <jmphair@gmail.com>
2023-01-09 21:03:46 -0500

    remember to use labels on your console logs

---
+++ tail.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let tailOfArray = [];
+  for (let i = 1; i < array.length; i++) {
+    console.log("---");
+    console.log("i", i);
+    console.log("val", array[i]);
+    tailOfArray.push(array[i]);
+  }
+  return tailOfArray;
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log("result", result);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
