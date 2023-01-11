fc0541a713091dbd57b1742df139b9e7684f5d2d
Jonathan Phair <jmphair@gmail.com>
2023-01-11 16:58:10 -0500

    with mentors additional help

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,31 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+//Pasted Function
+const assertEqual = function (actual, expected) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-//Test Code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//Actual Work
+const countLetters = function (str) {
+  const result = {};
+  const lowerCaseStr = str.toLowerCase();
+  for (let letter of lowerCaseStr) {
+    if (!result[letter]) {
+      result[letter] = 1;
+    } else {
+      result[letter]++;
+    }
+  }
+  return result;
+};
+
+// Test Code:
+
+console.log(countLetters("LHL"));
+console.log(assertEqual(countLetters("LHL"), { l: 2, h: 1 }));
+
+
+console.log(countLetters("lighthouse in the house"));
+console.log(assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, ' ': 3, n: 1 }));
