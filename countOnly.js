6da7fda00f5f6500e36d619291e04899a845e864
Jonathan Phair <jmphair@gmail.com>
2023-01-11 15:53:42 -0500

    cleaned it up a bit

--- countOnly.js
+++ countOnly.js
@@ -10,19 +10,10 @@ const assertEqual = function(actual, expected) {
 // Actual Work
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
+
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
-    //console.log(item);
-    // inside the loop,
-    // increment the counter for each item:
-    //   set a property with that string key to:
-    //     the value that was already there (or zero if nothing there) with 1 added to it.
-    // if (results[item]) {
-    //   results[item] += 1;
-    // } else {
-    //   results[item] = 1;
-    // }
     if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
@@ -50,8 +41,19 @@ const firstNames = [
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
+
+// REMOVED:
+   //console.log(item);
+    // inside the loop,
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+    // if (results[item]) {
+    //   results[item] += 1;
+    // } else {
+    //   results[item] = 1;
+    // }
