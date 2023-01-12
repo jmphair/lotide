6bfbd07ef4701feb421ea434fcb03d1be862eebd
Jonathan Phair <jmphair@gmail.com>
2023-01-11 19:56:32 -0500

    Initial Commit: on track to make it work

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+//PASTED
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//ACTUAL WORK
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (value === object[key]) {
+      console.log(key);
+      return value;
+    }
+  }
+  return;
+};
+
+
+//TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
