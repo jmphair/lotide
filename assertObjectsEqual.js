f1940d42a75cda4adb3048566b627a8cebe3ac1d
Jonathan Phair <jmphair@gmail.com>
2023-01-12 00:03:42 -0500

    seems to work as expected

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -19,3 +19,21 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
+
+//ACTUAL WORK
+
+// FUNCTION IMPLEMENTATION
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require(`util`).inspect;
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${inspect(ab)} !== ${inspect(ba)}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed:  ${inspect(ab)} === ${inspect(ba)}`);
+  }
+};
+
+//TEST
+const ab = { a: "1", b: "2"};
+const ba = { b: "2", a: "1"};
+console.log(assertObjectsEqual(eqObjects(ab,ba), true));
