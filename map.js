037994e583ce06db02c2c48bd5a70d5fa2437958
Jonathan Phair <jmphair@gmail.com>
2023-01-12 19:05:13 -0500

    wish they were all this straight forward

--- map.js
+++ map.js
@@ -1,2 +1,47 @@
-dsfsd
+//PASTED CODE
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  if (!result) {
+    console.log("🛑🛑🛑 whoops, these arrays are not equal");
+  } else {
+    console.log("✅✅✅ nice, these arrays are equal");
+  }
+};
+
+// Our MAP Function will take in two arguments:
+// 1. An array to map
+// 2. A callback function
+
+//ACTUAL WORK
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const results2 = map(words, word => word + word);
+console.log(results2);
+
+
+//TEST
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+assertArraysEqual(results2, [ 'groundground', 'controlcontrol', 'toto', 'majormajor', 'tomtom' ]);
