261ab9e85d7e520bbc8c9b6521f698f2bbf0fe2d
Jonathan Phair <jmphair@gmail.com>
2023-01-16 20:24:51 -0500

    added all the features to index object

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const map = require('./map');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./eqOBjects');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const swapper = require('./swapper');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  map: map,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  swapper: swapper,
+  takeUntil: takeUntil,
+  without: without,
+};
