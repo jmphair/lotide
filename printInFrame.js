9dc3a7aee6ba50d29a5d8f4c06b546c968e334f7
Jonathan Phair <jmphair@gmail.com>
2023-07-27 15:09:57 -0400

    add stretch debug

---
+++ printInFrame.js
@@ -0,0 +1,36 @@
+const printInFrame = function(list) {
+  list = list.split(' ');
+  const longest = longestStr(list).length;
+  const border = repeat('*', longest + 4);
+
+  console.log(border);
+  for (const word of list) {
+    const padding = longest - word.length;
+    console.log(`* ${word}${repeat(' ', padding)} *`);
+  }
+  console.log(border);
+};
+
+const repeat = function(str, times) {
+  let result = '';
+  for (let i = 0; i < times; i++) {
+    result += str;
+  }
+  return result;
+};
+
+const longestStr = function(list) {
+  let longest = list[0];
+  for (const str of list) {
+    if (str.length > longest.length) {
+      longest = str;
+    }
+  }
+  return longest;
+};
+
+// Test driver code, do not modify
+printInFrame('May the force be with you');
+printInFrame("Here's Johnny!");
+printInFrame('Supercalifragalisticexpialadocious');
+printInFrame('Lost, like tears in the rain');
